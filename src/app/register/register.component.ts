166c3e23e8b9d43cdb7e57b414fe45dc0fc1e6c4
lekpramual <lek.pramual@gmail.com>
2021-08-24 21:57:01 +0700

    update ReactiveFormsModule

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+
 import { FilterService } from "primeng/api";
 import { SelectItemGroup } from "primeng/api";
 import { UserService } from "../service/user.service";
+
+import packageInfo from '../../../package.json';
 
 @Component({
   selector: 'app-register',
@@ -32,17 +36,50 @@ export class RegisterComponent implements OnInit {
   valPhone!: string;
   valCid!: string;
 
+  loginForm!: FormGroup;
+  loading!: boolean;
+  version!: string;
+
   constructor(
+    public formBuilder: FormBuilder,
     private userService: UserService,
     private filterService: FilterService
   ) { }
 
   ngOnInit(): void {
 
+    this.resetForm();
+    this.version = packageInfo.version;
+
     this.userService.getUsers().then(countries => {
       this.countries = countries;
     });
   }
+
+  get getControl() {
+    return this.loginForm.controls;
+  }
+
+  public resetForm() {
+    this.loading = true
+
+    this.loginForm = this.formBuilder.group({
+      // fullname: ['', [Validators.required]],
+      fullname: { name: ['', [Validators.required]], code: ['', [Validators.required]] },
+      phone: ['', [Validators.required, Validators.minLength(10)]],
+      cid: ['', [Validators.required, Validators.minLength(13)]]
+    })
+  }
+
+  onSubmit() {
+    // delay progressSpinner 1 s 
+    console.log(this.loginForm.valid)
+    console.log(this.loginForm)
+    if (this.loginForm.valid) {
+      this.loading = false;
+    }
+  }
+
 
   filterCountry(event: { query: any; }) {
     //in a real application, make a request to a remote url with the query and return filtered results, for demo we filter at client side
