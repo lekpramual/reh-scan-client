35987aa6ee83c657f41a562c6846e17b09e9298a
lekpramual <lek.pramual@gmail.com>
2021-08-26 10:17:22 +0700

    update check in page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,7 +22,7 @@ const routes: Routes = [
       }
     ]
   },
-  
+
   // Auth Routes
   {
     path: '',
@@ -36,9 +36,7 @@ const routes: Routes = [
         path: '',
         redirectTo: '/register',
         pathMatch: 'full'
-      },
-      { path: '**', redirectTo: '/register' },
-
+      }
     ]
   }
 ];
