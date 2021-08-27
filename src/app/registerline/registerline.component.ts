5040408b92bc7c4614dd49fb8aaa92395dfa9fde
lekpramual <lek.pramual@gmail.com>
2021-08-27 15:06:21 +0700

    test url

--- src/app/registerline/registerline.component.ts
+++ src/app/registerline/registerline.component.ts
@@ -33,35 +33,43 @@ export class RegisterlineComponent implements OnInit {
     console.log(page);
     // // is param page
     if (page != null && page != '') {
-      // is login line and register
-      if (this.lineService.getUserIsLogin() && this.lineService.getCurrentUserIsLogin()) {
-        if (page === "checkin") {
-          this.router.navigate(['/checkin']);
-        } else if (page === "scanlist") {
-          this.router.navigate(['/scanlist']);
-        }
-      } else {
-        this.router.navigate(['/register']);
+      if (page === "checkin") {
+        console.log('checkin ....')
+      } else if (page === "scanlist") {
+        console.log('scanlist ....')
       }
     }
+
+    // if (page != null && page != '') {
+    //   // is login line and register
+    //   if (this.lineService.getUserIsLogin() && this.lineService.getCurrentUserIsLogin()) {
+    //     if (page === "checkin") {
+    //       this.router.navigate(['/checkin']);
+    //     } else if (page === "scanlist") {
+    //       this.router.navigate(['/scanlist']);
+    //     }
+    //   } else {
+    //     this.router.navigate(['/register']);
+    //   }
+    // }
     // is not param page
-    else {
-      // is login line
-      if (this.lineService.getUserIsLogin()) {
-        // is register
-        if (this.lineService.getCurrentUserIsLogin()) {
-          this.router.navigate(['/profile']);
-        }
-        // is not register
-        else {
-          this.router.navigate(['/register']);
-        }
-      }
-    }
+    // else {
+    //   // is login line
+    //   if (this.lineService.getUserIsLogin()) {
+    //     // is register
+    //     if (this.lineService.getCurrentUserIsLogin()) {
+    //       this.router.navigate(['/profile']);
+    //     }
+    //     // is not register
+    //     else {
+    //       this.router.navigate(['/register']);
+    //     }
+    //   }
+    // }
   }
 
   ngOnInit(): void {
-    this.getLiffLine();
+    // this.getLiffLine();
   }
 
   getLiffLine() {
@@ -72,7 +80,7 @@ export class RegisterlineComponent implements OnInit {
         console.log('id loggein ... ')
         liff.getProfile().then(profile => {
           this.profile = profile;
-          
+
           // บันทึกข้อมูล currentLine 
           console.log('login success...');
           localStorage.setItem('currentLine', JSON.stringify(this.profile));
