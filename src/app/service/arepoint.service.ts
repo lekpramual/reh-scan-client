09a0c36c17de8b71401433214f9b613a5848b6fd
lekpramual <lek.pramual@gmail.com>
2021-08-31 13:06:30 +0700

    update area point

--- src/app/service/arepoint.service.ts
+++ src/app/service/arepoint.service.ts
@@ -8,11 +8,15 @@ export class ArepointService {
   constructor() { }
 
   arePointsNear(checkPoint: { lat: number; lng: number; }, centerPoint: { lat: number; lng: number; }) {
-    var ky = 40000 / 360;
+    var ky = 4000 / 360;
     var kx = Math.cos((Math.PI * centerPoint.lat) / 180.0) * ky;
     var dx = Math.abs(centerPoint.lng - checkPoint.lng) * kx;
     var dy = Math.abs(centerPoint.lat - checkPoint.lat) * ky;
-    console.log(dx * dx + dy * dy);
-    return Math.sqrt(dx * dx + dy * dy) <= 0.01;
+
+    console.log(dx * dx);
+    console.log(dy * dy)
+    console.log(dx * dx + dy * dy)
+    console.log(Math.sqrt(dx * dx + dy * dy) <= 1);
+    return Math.sqrt(dx * dx + dy * dy) <= 0.2;
   }
 }
