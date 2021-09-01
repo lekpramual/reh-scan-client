f6ad8ac549cea15259dcc3ee015aaa52e6e34d67
lekpramual <lek.pramual@gmail.com>
2021-09-01 09:51:26 +0700

    update are point

--- src/app/checkin/checkin.component.ts
+++ src/app/checkin/checkin.component.ts
@@ -40,6 +40,9 @@ export class CheckinComponent implements OnInit {
     if (!this.lineService.getUserIsLogin() && !this.lineService.getCurrentUserIsLogin()) {
       this.router.navigate(['/']);
     }
+
+    // load location
+    this.getLocaton();
   }
   ngOnInit() {
     this.primengConfig.ripple = true;
@@ -48,8 +51,6 @@ export class CheckinComponent implements OnInit {
     this.pictureUrl = this.lineService.getUserValue().pictureUrl;
     this.userId = this.lineService.getUserValue().userId;
     this.displayName = this.lineService.getUserValue().displayName;
-
-    this.getLocaton();
 
 
     this.route.params.subscribe((params: Params) => {
@@ -72,19 +73,17 @@ export class CheckinComponent implements OnInit {
 
 
   isCheckArePoint() {
-
     // { lat2: 16.0487079, lon2: 103.6510467 } จุดกึ่งกลาง
     // { lat1: 16.0486144, lon1: 103.6505200 } จุดสแกน นอกศูนย์คอม | 3.2183401536933527
     // { lat1: 16.048707960017705, lon1: 103.65104675009971 }, ห้องประชุม ชั้น 7 |  54.492843409208106
-
     return this.arepointService.testFun(
-      { lat1: this.lat, lon1: this.lng }, { lat2: 16.04861893399044, lon2: 103.65054529523633 }
+      { lat1: this.lat, lon1: this.lng }, { lat2: 16.04870796002785, lon2: 103.65104675015687 }
     )
   }
 
   isCheckArePoint1() {
     return this.arepointService.testFun1(
-      { lat1: this.lat, lon1: this.lng }, { lat2: 16.04861893399044, lon2: 103.65054529523633 }
+      { lat1: this.lat, lon1: this.lng }, { lat2: 16.04870796002785, lon2: 103.65104675015687 }
     )
   }
 
--- src/app/service/arepoint.service.ts
+++ src/app/service/arepoint.service.ts
@@ -8,83 +8,6 @@ export class ArepointService {
 
   constructor() { }
 
-  arePointsNear(checkPoint: { lat: number; lng: number; }, centerPoint: { lat: number; lng: number; }) {
-    var ky = 4000 / 360;
-    var kx = Math.cos((Math.PI * centerPoint.lat) / 180.0) * ky;
-    var dx = Math.abs(centerPoint.lng - checkPoint.lng) * kx;
-    var dy = Math.abs(centerPoint.lat - checkPoint.lat) * ky;
-
-    console.log(dx * dx);
-    console.log(dy * dy)
-    console.log(dx * dx + dy * dy)
-    console.log(Math.sqrt(dx * dx + dy * dy) <= 0.1);
-    return Math.sqrt(dx * dx + dy * dy) <= 0.1;
-  }
-
-  distance(checkPoint: { lat1: number; lon1: number; }, centerPoint: { lat2: number; lon2: number; }) {
-    // lat1, lon1, lat2, lon2
-    /* var R = 6371; // Earth's radius in Km
-    return Math.acos(Math.sin(checkPoint.lat1) * Math.sin(centerPoint.lat2) +
-    Math.cos(checkPoint.lat1) * Math.cos(centerPoint.lat2) *
-    Math.cos(centerPoint.lon2 - checkPoint.lon1)) * R;
-    */
-
-    const lat = checkPoint.lat1 - centerPoint.lat2
-
-    // distance(user.lat, user.lon, post.lat, post.lon) <= desiredRadiusInKm
-  }
-
-  degreesToRadians(degrees: number) {
-    return degrees * Math.PI / 180;
-  }
-
-  distanceInKmBetweenEarthCoordinates(checkPoint: { lat1: number; lon1: number; }, centerPoint: { lat2: number; lon2: number; }) {
-
-    var earthRadiusKm = 6371;
-
-    var dLat = this.degreesToRadians(centerPoint.lat2 - checkPoint.lat1);
-    var dLon = this.degreesToRadians(centerPoint.lon2 - checkPoint.lon1);
-
-    var lat1 = this.degreesToRadians(checkPoint.lat1);
-    var lat2 = this.degreesToRadians(centerPoint.lat2);
-
-    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-      Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    return earthRadiusKm * c;
-  }
-
-  /**
- * Calculates the great-circle distance between two points, with
- * the Haversine formula.
- * @param float $latitudeFrom Latitude of start point in [deg decimal]
- * @param float $longitudeFrom Longitude of start point in [deg decimal]
- * @param float $latitudeTo Latitude of target point in [deg decimal]
- * @param float $longitudeTo Longitude of target point in [deg decimal]
- * @param float $earthRadius Mean earth radius in [m]
- * @return float Distance between points in [m] (same as earthRadius)
- */
-  haversineGreatCircleDistance(
-    checkPoint: { lat1: number; lon1: number; }, centerPoint: { lat2: number; lon2: number; }
-    // $latitudeFrom, $longitudeFrom, $latitudeTo, $longitudeTo, $earthRadius = 6371000
-
-  ) {
-    // convert from degrees to radians
-    var latFrom = this.degreesToRadians(checkPoint.lat1);
-    var lonFrom = this.degreesToRadians(checkPoint.lon1);
-    var latTo = this.degreesToRadians(centerPoint.lat2);
-    var lonTo = this.degreesToRadians(centerPoint.lon2);
-
-    var latDelta = latTo - latFrom;
-    var lonDelta = lonTo - lonFrom;
-
-    var angle = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(latDelta / 2), 2) +
-      Math.cos(latFrom) * Math.cos(latTo) * Math.pow(Math.sin(lonDelta / 2), 2)));
-
-    console.log(angle)
-    return angle * 6371000;
-  }
-
   testFun(checkPoint: { lat1: number; lon1: number; }, centerPoint: { lat2: number; lon2: number; }) {
 
     // isPointWithinRadius(point, centerPoint, radius) ตรวจสอบว่าจุดอยู่ภายในวงกลมหรือไม่
@@ -95,7 +18,9 @@ export class ArepointService {
     }, {
       latitude: centerPoint.lat2,
       longitude: centerPoint.lon2,
-    }, geolib.convertDistance(10, 'm'))
+    }, 1)
+
+    // geolib.convertDistance(10, 'm')
   }
 
   testFun1(checkPoint: { lat1: number; lon1: number; }, centerPoint: { lat2: number; lon2: number; }) {
@@ -108,7 +33,7 @@ export class ArepointService {
     }, {
       latitude: centerPoint.lat2,
       longitude: centerPoint.lon2,
-    })
+    }, 1)
   }
 
 
